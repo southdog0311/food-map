b0ac7e69bd5ebe6231a32d6c6f1a69cec25d5050
southdog0311 <sam2227219@gmail.com>
2024-08-21 05:59:12 +0000

    Initial commit

--- food_map/mysite/settings.py
+++ food_map/mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1^+r037%_k^hwjmh=9fr^*94ks@x8f1*66m!9m!@!-dylg#8u1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['southdog0311.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
@@ -118,9 +118,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://southdog0311.pythonanywhere.com']
+
+
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
