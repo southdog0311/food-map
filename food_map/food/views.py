1ecaf30a84fa8075026dc45cf2624bac8a79cbf5
徐文献 <sam2227219@gmail.com>
2024-09-04 08:28:33 +0000

    tag form

--- food_map/food/views.py
+++ food_map/food/views.py
@@ -4,20 +4,23 @@ from django.views.generic import ListView
 
 def index(request):
     places = Place.objects.all()
-    return render(request, 'food/index.html', {'places': places})
+    tags = Tag.objects.all()  # 傳遞標籤列表給模板
+    return render(request, 'food/index.html', {'places': places, 'tags': tags})
 
 def place_detail(request, place_id):
     place = get_object_or_404(Place, pk=place_id)
     return render(request, 'food/place_detail.html', {'place': place})
-    
+
 def search(request):
     query = request.GET.get('search')
     restaurant_name = request.GET.get('restaurant_name')
     cuisine_type = request.GET.get('cuisine_type')
     address = request.GET.get('address')
+    tag_ids = request.GET.getlist('tags')
 
     search_results = Place.objects.all()
 
+    # 每個篩選條件獨立應用，允許條件為空
     if query:
         search_results = search_results.filter(name__icontains=query)
 
@@ -30,8 +33,12 @@ def search(request):
     if address:
         search_results = search_results.filter(address__icontains=address)
 
+    if tag_ids:
+        search_results = search_results.filter(tags__id__in=tag_ids).distinct()
+
     context = {
         'places': search_results,
+        'tags': Tag.objects.all(),  # 傳遞標籤列表給模板
     }
     return render(request, 'food/search_results.html', context)
 
