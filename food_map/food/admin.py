f5ff067e1a1521f2d104bf3407991304bba982e0
徐文献 <sam2227219@gmail.com>
2024-09-06 05:25:14 +0000

    multi tag

--- food_map/food/admin.py
+++ food_map/food/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
-from .models import  Place, Tag, TagManagement
+from .models import Place, Tag, TagManagement
+
+class TagInlineAdmin(admin.StackedInline):
+    model = Place.tags.through
+
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'address', 'website', 'pub_date')
+    list_filter = ('pub_date', 'tags')
+    search_fields = ('name', 'address', 'website')
+    ordering = ('-id',)
+    readonly_fields = ('pub_date',)
+    inlines = [TagInlineAdmin]
 
 admin.site.register(Tag)
-admin.site.register(Place)
+admin.site.register(Place, PlaceAdmin)
 admin.site.register(TagManagement)
